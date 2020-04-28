91fa0541b2bbb0b2d693d7273ad9edcbfa846200
Arjun Nair <nair.arjun47@yahoo.com>
2020-04-28 14:57:21 -0700

    New unit test works

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -11,8 +11,12 @@ namespace Gradebook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-            book.ShowStatistics();
-            // Making change to push due to merge conflict.
+            
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
         }
     }
 }
