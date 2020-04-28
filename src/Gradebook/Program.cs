22ce854dbc55aed1a6fead64f457263ba5549dfd
Arjun Nair <nair.arjun47@yahoo.com>
2020-04-27 23:51:54 -0700

    Create list

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -6,10 +7,12 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            var numbers = new []{12.7, 10.3, 6.11, 4.1};
+            var numbers = new [] {12.7, 10.3, 6.11, 4.1};
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
 
             var result = 0.0;
-            foreach(var number in numbers)
+            foreach(var number in grades)
             {
                 result += number;
             }
