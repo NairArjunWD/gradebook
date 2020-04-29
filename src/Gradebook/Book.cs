14f8d39e37691da1e781ca1ae57cff42015dc8b3
Arjun Nair <nair.arjun47@yahoo.com>
2020-04-28 23:37:14 -0700

    Improved letter grades

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -30,7 +30,7 @@ namespace Gradebook
                 default:
                     AddGrade(0);
                     break;
-                    
+
             }
         }
 
@@ -60,6 +60,29 @@ namespace Gradebook
                 result.Average += grades[index];
             }
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
