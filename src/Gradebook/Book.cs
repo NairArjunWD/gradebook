5e87de345db541f9c7976646337cdfa9c0e6b064
Arjun Nair <nair.arjun47@yahoo.com>
2020-04-28 21:14:44 -0700

    TypeTest created

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -8,7 +8,7 @@ namespace Gradebook
         public Book(string name)
         {
             grades = new List<double>();
-            this.name = name;
+            Name = name;
         }
 
         public void AddGrade(double grade)
@@ -35,6 +35,6 @@ namespace Gradebook
         }
 
         private List<double> grades;
-        private string name;
+        public string Name;
     }
 }
