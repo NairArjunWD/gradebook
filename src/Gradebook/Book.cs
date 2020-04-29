5183120da57ee3aedd8f58dce539c5347a25bc55
Arjun Nair <nair.arjun47@yahoo.com>
2020-04-28 23:22:12 -0700

    Change AddGrade in Book file

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -13,7 +13,14 @@ namespace Gradebook
 
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
         }
 
         public Statistics GetStatistics()
